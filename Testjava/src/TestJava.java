5a86849c3f5bd5c0b76f20f68879fb5b3576fcdc
KimWoojin <KimWoojin@DESKTOP-6R7GO3N>
2021-12-03 14:54:20 +0900

    김우진 함수추가

--- Testjava/src/TestJava.java
+++ Testjava/src/TestJava.java
@@ -2,5 +2,12 @@
 public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!!");
+		
+		
+	}
+	
+
+public void test() {
+	System.out.println("test!");
 	}
 }
